2c26e933a16ed512e0b2496fe62ae597790ae69a
William Santiago Alzate <santiago_alza@hotmail.com>
2020-07-02 18:53:43 -0500

    Curso de angular basico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+import { Employee } from './models/employee';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  employeeArray: Employee[] = [
+    {id: 1, name: "Willi", country: "CO"},
+    {id: 2, name: "Boss", country: "CO"},
+    {id: 3, name: "James", country: "USA"},
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
+
+  openForEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+
+  delete() {
+    if (confirm('Are you sure you want to delete it?')) {
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+  }
 }
